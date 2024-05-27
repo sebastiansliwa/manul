02d2f4991a49b05664ab91566fcc631127ab5f7d
Sebastian Śliwa <sebastiansliwa_23@wp.pl>
2024-05-27 18:46:14 +0200

    Add flutterfire_ui

---
+++ lib/features/home/pages/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:manul/features/auth/pages/auth_gate.dart';
+//import 'firebase_options.dart';
+
+class HomePage extends StatelessWidget {
+   HomePage({
+    super.key,
+  });
+
+final myfield = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+           appBar: AppBar(
+          title: const Text('Wyszukaj'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('services')
+                .add({'title': myfield.text});
+            myfield.clear();
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection('services').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Wystąpił nieoczekiwany problem');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text('Trwa ładowanie');
+              }
+              final services = snapshot.data!.docs;
+              return ListView(
+                children: [
+                  for (final service in services) ...[
+                    ListViewItem(service['title']),
+                  ],
+                  TextField(
+                    controller: myfield,
+                    style: const TextStyle(color: Colors.white70),
+                  ),
+                ],
+              );
+            }),
+      
+      );
+  }
+}
+
+class ListViewItem extends StatelessWidget {
+  const ListViewItem(
+    this.title, {
+    super.key,
+  });
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      color: const Color.fromARGB(255, 245, 245, 245),
+      padding: const EdgeInsets.only(
+        right: 20,
+      ),
+      margin: const EdgeInsets.only(
+        top: 5,
+      ),
+      child: Row(
+        children: [Text(title)],
+      ),
+    );
+  }
+}

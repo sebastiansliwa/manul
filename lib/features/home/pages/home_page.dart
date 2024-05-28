7257e50b2d8c86f45344254269c1962b4da88f6f
Sebastian Śliwa <sebastiansliwa_23@wp.pl>
2024-05-28 18:22:42 +0200

    Implement log out

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,56 +1,63 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:manul/features/auth/pages/auth_gate.dart';
 //import 'firebase_options.dart';
 
 class HomePage extends StatelessWidget {
-   HomePage({
+  HomePage({
     super.key,
   });
 
-final myfield = TextEditingController();
+  final myfield = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-           appBar: AppBar(
-          title: const Text('Wyszukaj'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            FirebaseFirestore.instance
-                .collection('services')
-                .add({'title': myfield.text});
-            myfield.clear();
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: StreamBuilder<QuerySnapshot>(
-            stream:
-                FirebaseFirestore.instance.collection('services').snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return const Text('Wystąpił nieoczekiwany problem');
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text('Trwa ładowanie');
-              }
-              final services = snapshot.data!.docs;
-              return ListView(
-                children: [
-                  for (final service in services) ...[
-                    ListViewItem(service['title']),
-                  ],
-                  TextField(
-                    controller: myfield,
-                    style: const TextStyle(color: Colors.white70),
-                  ),
+      appBar: AppBar(
+        title: const Text('Wyszukaj'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(Icons.logout_sharp),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('services')
+              .add({'title': myfield.text});
+          myfield.clear();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('services').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Wystąpił nieoczekiwany problem');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Trwa ładowanie');
+            }
+            final services = snapshot.data!.docs;
+            return ListView(
+              children: [
+                for (final service in services) ...[
+                  ListViewItem(service['title']),
                 ],
-              );
-            }),
-      
-      );
+                TextField(
+                  controller: myfield,
+                  style: const TextStyle(color: Colors.white70),
+                ),
+              ],
+            );
+          }),
+    );
   }
 }
 
